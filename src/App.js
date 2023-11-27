4ce21f659e667a71c1545b6eede3dfab03683ef5
Grasiele Pacheco <grasiele2191@gmail.com>
2023-11-27 12:25:47 +0100

    tried react a little further

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article_title">Replace me</h2>
+      <label htmlFor="name">test</label>
+      <input id="name" type="text" />
+      <a className="article_name" href="">
+        whoknows
+      </a>
+    </article>
+  );
 }
